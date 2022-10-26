d73ba44b1e1e9ea1736afd65c2de561e8555bd75
Aleksandr <popkov.aleks.v@yandex.ru>
2022-10-26 19:22:35 +0500

    Add model for images

--- where_to_go/places/models.py
+++ where_to_go/places/models.py
@@ -1,3 +1,4 @@
+from distutils.command.upload import upload
 from unittest.util import _MAX_LENGTH
 from django.db import models
 
@@ -15,3 +16,14 @@ class Place(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Image(models.Model):
+    name = models.CharField(max_length=200)
+
+    order_num = models.IntegerField(default=1)
+
+    image = models.ImageField()
+
+    def __str__(self):
+        return f'{self.order_num} {self.name}'
