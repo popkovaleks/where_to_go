b7b2641292775c9d8b503b6bc7360d7a32b37885
Aleksandr <popkov.aleks.v@yandex.ru>
2022-10-27 16:25:11 +0500

    Inline for images in Places

--- where_to_go/places/admin.py
+++ where_to_go/places/admin.py
@@ -2,10 +2,16 @@ from django.contrib import admin
 
 from places.models import Place, Image
 
+
+class ImageInline(admin.TabularInline):
+    model = Image
+
 @admin.register(Place)
 class PlaceAdmin(admin.ModelAdmin):
     list_display = ('id', 'title_short')
     list_display_links = ('title_short',)
+
+    inlines = [ImageInline,]
 
 @admin.register(Image)
 class ImageAdmin(admin.ModelAdmin):
