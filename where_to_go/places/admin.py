79d16f0603338e380a490acb67fd3b713b38c8e3
Aleksandr <popkov.aleks.v@yandex.ru>
2022-10-28 00:40:17 +0500

    Added Image preview

--- where_to_go/places/admin.py
+++ where_to_go/places/admin.py
@@ -1,10 +1,21 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
 from places.models import Place, Image
 
 
 class ImageInline(admin.TabularInline):
     model = Image
+
+    fields = ['image', 'image_preview', 'order_num']
+    readonly_fields = ['image_preview', ]
+
+    def image_preview(self, obj):
+        return mark_safe("<img src=\"{url}\" height={height})>".format(
+            url=obj.image.url,
+            height=200
+        ))
+
 
 @admin.register(Place)
 class PlaceAdmin(admin.ModelAdmin):
