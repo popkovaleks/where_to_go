9be08f095e75856f7180d4108cdb10abec034097
Aleksandr <popkov.aleks.v@yandex.ru>
2022-11-01 23:15:26 +0500

    trailing slash

--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -9,6 +9,6 @@ from where_to_go import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
-    path('places/<int:id>', views.detail_data, name='detail_data'),
+    path('places/<int:id>/', views.detail_data, name='detail_data'),
     path('tinymce/', include('tinymce.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
