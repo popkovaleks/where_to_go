190ef8981813f55a89d82dcbf7c7bde84e411b3e
Aleksandr <popkov.aleks.v@yandex.ru>
2022-11-05 11:59:29 +0500

    Image now in admin

--- places/admin.py
+++ places/admin.py
@@ -5,17 +5,20 @@ from adminsortable2.admin import SortableInlineAdminMixin
 from places.models import Place, Image
 
 
-class ImageInline(SortableInlineAdminMixin, admin.TabularInline):
-    model = Image
-
-    fields = ['image', 'image_preview', 'order_num']
+class BasicImageAdmin():
+    fields = ['order_num', 'place', 'image', 'image_preview']
     readonly_fields = ['image_preview', ]
-
     def image_preview(self, obj):
-        return mark_safe("<img src=\"{url}\" height={height})>".format(
+        return mark_safe('<img src="{url}" height={height})>'.format(
             url=obj.image.url,
             height=200
         ))
+
+
+class ImageInline(BasicImageAdmin, SortableInlineAdminMixin, admin.TabularInline):
+    model = Image
+
+    fields = ['image', 'image_preview', 'order_num', ]
 
 
 @admin.register(Place)
@@ -24,3 +27,8 @@ class PlaceAdmin(admin.ModelAdmin):
     list_display_links = ('title', )
     search_fields = ['title', ]
     inlines = [ImageInline, ]
+
+
+@admin.register(Image)
+class ImageAdmin(BasicImageAdmin, admin.ModelAdmin):
+    pass
