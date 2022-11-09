39b64b905e3c2a8ca9375b71ccdd4c3d07786984
Aleksandr <popkov.aleks.v@yandex.ru>
2022-11-09 10:28:27 +0500

    processing errors

--- places/management/commands/load_place.py
+++ places/management/commands/load_place.py
@@ -15,28 +15,35 @@ class Command(BaseCommand):
         address = kwargs['link']
         response = requests.get(address)
 
-        if response.status_code != 200:
-            print(f'status: {response.status_code}, text: {response.text}')
-            return
-
-        response = response.json()
-        place, created = Place.objects.get_or_create(
-            title=response.get('title', 'Введите название!'),
-            defaults={
-                'description_short': response.get('description_short', ''),
-                'description_long': response.get('description_long', ''),
-                'lng': response.get('coordinates').get('lng'),
-                'lat': response.get('coordinates').get('lat')
-                }
-        )
-
-        if created and response.get('imgs'):
-            for i, img_link in enumerate(response.get('imgs'), start=1):
-
-                resp_img = requests.get(img_link)
-                img, created_img = Image.objects.get_or_create(
-                    place=place,
-                    order_num=i,
-                    image=ContentFile(resp_img.content,
-                                      name=img_link.split('/')[-1])
+        if response.ok:
+            response = response.json()
+            try:
+                place, created = Place.objects.get_or_create(
+                    title=response['title'],
+                    defaults={
+                        'description_short': response.get('description_short', ''),
+                        'description_long': response.get('description_long', ''),
+                        'lng': response['coordinates']['lng'],
+                        'lat': response['coordinates']['lat']
+                        }
                 )
+            except KeyError:
+                print('Title or coordinates was not set')
+                return
+
+            if created and response.get('imgs'):
+                for i, img_link in enumerate(response.get('imgs', []), start=1):
+
+                    resp_img = requests.get(img_link)
+                    if resp_img.ok:
+                        img, created_img = Image.objects.get_or_create(
+                            place=place,
+                            order_num=i,
+                            image=ContentFile(resp_img.content,
+                                              name=img_link.split('/')[-1])
+                        )
+                    else:
+                        print(f'{i} {img_link} \
+                              status: {resp_img.status_code} {resp_img.text}')
+        else:
+            print(f'status: {response.status_code}, text: {response.text}')
